ef2f7855a2c4ec3721138c204315e2b3e1fbc179
dattebayoRob <robson.william65@gmail.com>
2019-04-14 12:46:42 -0300

    First Component

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (props) => 
+    <div>
+        <h1>
+            {props.value}
+        </h1>
+        <h2>
+            {props.subValue}
+        </h2>
+    </div>
+    ;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import First from './components/FirstComponent';
 
 const element = document.getElementById('root');
-ReactDOM.render('Hello React', element);
+ReactDOM.render(
+    <div>
+        <First value="First Component" subValue="Hey, I'm Using this component"></First>
+    </div>
+    , element);
