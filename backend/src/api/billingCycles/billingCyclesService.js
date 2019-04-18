c19a8fdcd2786f3dd993ed3457aecb833bb6febe
dattebayoRob <robson.william65@gmail.com>
2019-04-18 15:48:56 -0300

    Mymoney-app Api. Finish api

--- backend/src/api/billingCycles/billingCyclesService.js
+++ backend/src/api/billingCycles/billingCyclesService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycles')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new: true, runValidators: true}) //By Default, validations are only on post
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count',(req,res,next) => {
     BillingCycle.count((error,value) => {
@@ -12,5 +14,22 @@ BillingCycle.route('count',(req,res,next) => {
         }
     })
 })
+BillingCycle.route('summary', (req,res,next)=>{
+    BillingCycle.aggregate(
+        [{
+            $project: {credit: {$sum: "$credits.value"}, debit: { $sum: "$debits.value"}}
+        },{
+            $group: {_id: null, credit: {$sum: "$credit"}, debit: {$sum: "$debit"}}
+        },{
+            $project: {_id: 0, credit: 1, debit: 2}
+        }],(error, result) => {
+            if(error){
+                res.status(500).json({errors: [error]})
+            }else{
+                res.json(result[0] || {credit: 0, debit: 0})
+            }
+        }
+    )
+}) 
 
 module.exports = BillingCycle
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://admin:qwe123@127.0.0.1:27017/mymoneyapp');
+module.exports = mongoose.connect(
+    'mongodb://admin:qwe123@127.0.0.1:27017/mymoneyapp',
+    { useNewUrlParser: true },
+    err => {
+        if (err) {
+          console.log(`[SERVER_ERROR] MongoDB Connection:`, err);
+        }
+      }
+    )
 
 mongoose.Error.messages.general.required = "The attribut '{PATH}' is required"
+mongoose.Error.messages.Number.min = 
+    "The '{VALUE}' is lower then minimun required of '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' is greater then max permited of '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' isnt a valid attribut '{PATH}'."
