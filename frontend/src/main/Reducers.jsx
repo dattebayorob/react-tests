49327c4389518cd297e74ef78402e6f06d555c51
dattebayoRob <robson.william65@gmail.com>
2019-04-20 12:59:02 -0300

    Wrap App on Provider with stored reducers

---
+++ frontend/src/main/Reducers.jsx
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import DashboardReducer from '../dashboard/DashboardReducer'
+
+const rootReducer = combineReducers(
+    {
+        dashboard: DashboardReducer
+    }
+)
+
+export default rootReducer
