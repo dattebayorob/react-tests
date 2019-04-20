0fd62f9b4f0719f3c95ee0dfba61bcb1d5f0b814
dattebayoRob <robson.william65@gmail.com>
2019-04-20 15:47:31 -0300

    Tab navs

--- frontend/src/billingCycles/BillingCycles.jsx
+++ frontend/src/billingCycles/BillingCycles.jsx
@@ -1,17 +1,48 @@
 import React from 'react'
 import { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Content from '../common/template/Content'
 import ContentHeader from '../common/template/ContentHeader'
+import Tabs from '../common/tab/Tabs'
+import TabHeader from '../common/tab/TabHeader'
+import TabsHeader from '../common/tab/TabsHeader'
+import TabsContent from '../common/tab/TabsContent'
+import TabContent from '../common/tab/TabContent'
+import {selectTab,showTabs} from '../common/tab/TabActions'
 
-export default class extends Component{
+
+class BillingCycles extends Component{
+    componentWillMount() {
+        if(!this.props.tab.selected)
+            this.props.selectTab('tabList')
+        this.props.showTabs('tabList','tabCreate')
+    }
     render(){
         return (
             <div>
                 <ContentHeader title="Billing Cycles" small="Checkout your pocket"/>
                 <Content>
-                    Billing Cycles
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label="List" icon="bars" target="tabList" />
+                            <TabHeader label="Insert" icon="plus" target="tabCreate" />
+                            <TabHeader label="Updated" icon="pencil" target="tabUpdate" />
+                            <TabHeader label=" Delete" icon="trash" target="tabDelete" />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id="tabList">List</TabContent>
+                            <TabContent id="tabCreate">Create</TabContent>
+                            <TabContent id="tabUpdate">Update</TabContent>
+                            <TabContent id="tabDelete">Delete</TabContent>
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => ({tab: state.tab})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab,showTabs}, dispatch)
+export default connect(mapStateToProps,mapDispatchToProps)(BillingCycles)
